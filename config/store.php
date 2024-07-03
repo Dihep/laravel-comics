fe6420fa89b786a7561b68c4a9e13554aed80580
Dihep <dihep.demayo@gmail.com>
2024-07-03 23:26:34 +0200

    M2 - Creazione e test dello store con i dati.

---
+++ config/store.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'prova1' => 'Prima stringa di prova(2)',
+    'prova2' => 'Seconda stringa di prova(2)',
+    'prova3' => 'Terza stringa di prova(2)'
+];
--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,11 @@ use Illuminate\Support\Facades\Route;
 // Rotte
 
 Route::get('/', function () {
-    // Creazione dati
-    $data = [
-        'prova1' => 'Prima stringa di prova',
-        'prova2' => 'Seconda stringa di prova',
-        'prova3' => 'Terza stringa di prova'
-    ];
-
+    $data = config('store');
     return view('homepage', $data);
 });
 
 Route::get('/home', function () {
-    // Creazione dati
-    $data = [
-        'prova1' => 'Prima stringa di prova',
-        'prova2' => 'Seconda stringa di prova',
-        'prova3' => 'Terza stringa di prova'
-    ];
-
+    $data = config('store');
     return view('homepage', $data);
 });
