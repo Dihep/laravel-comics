fe661e52c5d4a6167978ae99363031cf714fe9fe
Dihep <dihep.demayo@gmail.com>
2024-07-08 00:14:53 +0200

    E2 - Creazione layout e componenti pagina.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="/style.css">
-</head>
+@extends('layouts.homeLayout')
 
+@section('content')
 
-<body>
-
-    <h1>Questa è la homepage</h1>
-
-    <ul>
-        @foreach ($comics as $indice => $fumetto)
+@foreach ($comics as $indice => $fumetto)
         <div class="cardFumetto">
             <ul>
                 <li><h1>{{$fumetto['titolo']}}</h1></li>
@@ -25,9 +13,5 @@
             </ul>
         </div>            
         @endforeach
-        <li>Prove terminate! Vedere i crediti (/about)</li>
 
-    </ul>
-
-</body>
-</html>
+@endsection
